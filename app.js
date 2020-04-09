977d182c06baf2c087b960080636642dfbc31078
“UrmnafBortHyuga” <yurihyuga1889@gmail.com>
2020-04-09 17:21:48 +0900

    change CROS Origin

--- app.js
+++ app.js
@@ -11,12 +11,31 @@ var testAPIRouter = require('./routes/testAPI');
 var app = express();
 
 
+
 // CORSを許可する
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+
+app.use(allowCrossDomain)
 
 // app.use((req, res, next) => {
 //   // CORSのためのhttpリクエストヘッダー設定
